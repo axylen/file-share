477ff8ace52e19c8b94f8170565e05dc86b9e554
axylen <jeka2644@gmail.com>
2020-04-13 13:09:38 +0300

    Updates

---
+++ precache-manifest.d82d647a8e78436bc1c44a5949e340b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9fc6d0bfa42476a24e7f722d4ed124",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9099fc0d5844ff80e62d",
+    "url": "./static/css/main.82948d08.chunk.css"
+  },
+  {
+    "revision": "dfc574e8e98079ff8a7e",
+    "url": "./static/js/2.cddfc3fc.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "./static/js/2.cddfc3fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9099fc0d5844ff80e62d",
+    "url": "./static/js/main.7687bab0.chunk.js"
+  },
+  {
+    "revision": "f26d5082e45746fff761",
+    "url": "./static/js/runtime-main.bdd97978.js"
+  }
+]);
