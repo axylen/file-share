7504e5ff6452eb50c509a5287882cda77a9da6e8
axylen <jeka2644@gmail.com>
2020-08-11 17:57:07 +0300

    Updates

--- precache-manifest.450742eeac63e286d56d863aa65c7688.js
+++ precache-manifest.450742eeac63e286d56d863aa65c7688.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b124961f43685111c3c74008bedb4ff",
+    "revision": "c859361e04484f44dadae16b99dc2a8b",
     "url": "./index.html"
   },
   {
-    "revision": "eed20b1b68ad4ed783ae",
+    "revision": "55b033bdb6a428054930",
     "url": "./static/css/main.e15e57df.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.60947212.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eed20b1b68ad4ed783ae",
-    "url": "./static/js/main.526c5450.chunk.js"
+    "revision": "55b033bdb6a428054930",
+    "url": "./static/js/main.e6dd4a83.chunk.js"
   },
   {
     "revision": "f26d5082e45746fff761",
